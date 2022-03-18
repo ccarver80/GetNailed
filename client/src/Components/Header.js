1e09b9fb61cf22b214eca824b6f40680cec19f28
ccarver80 <ckc_80@outlook.com>
2022-03-17 23:23:34 -0600

    Installed tailwind and messing around

---
+++ client/src/Components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+    return(
+        <div className='w-screen h-[100px] bg-cyan-400 fixed drop-shadow-lg' >
+           <h1 className="text-6xl text-shadow text-center font-Shadows">Get Nailed</h1>
+        </div>
+        
+    )
+}
---
+++ client/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'Shadows': ['"Shadows Into Light"', 'cursive']
+      }
+    },
+  },
+  plugins: [],
+}
