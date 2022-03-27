81f37209311970db1253dc31f89fc16ed2d3e72f
ccarver80 <ckc_80@outlook.com>
2022-03-26 21:17:41 -0600

    added more color

--- client/src/Components/Main.js
+++ client/src/Components/Main.js
@@ -8,13 +8,13 @@ import images from './Imgs'
 
 export default function About() {
   return (
-    <>
+    <div className=" bg-lime-200">
       <Header />
       {/* ============================== ABOUT ME ====================================== */}
 
-      <div className="lg:flex md:flex block shadow-purple-500 shadow-lg bg-slate-400 p-5 mx-3 mt-7 ">
+      <div className="lg:flex md:flex block shadow-purple-500 shadow-lg bg-slate-400 p-5 mx-3 mt-10 ">
         <img className="lg:h-96 md:h-56 mr-5 mb-3" src={Mandi} />
-        <div className="mx-auto my-auto bg-lime-50 p-5 lg:w-[35rem]  shadow-purple-500 shadow-md  text-center">
+        <div className="mx-auto my-auto bg-lime-50 p-5 lg:w-[35rem] rounded-xl shadow-purple-500 shadow-md  text-center">
           <h1 className="text-4xl mx-auto my-auto">Hello, I'm Mandi Watson</h1>
           <p className="mx-auto my-auto text-justify text-xl mt-2">
             I have been doing nails in Forsyth Montana for 11 years. I have recently started the venture of Press On Nails so the 
@@ -24,16 +24,18 @@ export default function About() {
       </div>
 
       {/* ================================ Photo Gallery ======================================== */}
-        <div className="flex shadow-purple-500 shadow-lg bg-slate-700 p-10 mt-5 mx-5 ">
+        <div className="flex shadow-purple-500 shadow-lg bg-slate-400 p-10 mt-10 mx-5 ">
         <div className="mx-auto sm:w-auto w-[200px]">
         <h1 className="text-4xl text-center text-white mb-2">Gallery</h1>
         <ImageGallery items={images}  />
         </div>
         </div>
 
-        <div className="text-center h-screen bg-slate-700 mt-5 mx-5">
+
+      {/* ================================== Store =============================================== */}
+        <div className="text-center h-screen bg-slate-400 mt-10 mx-5">
           <h1 className= "text-4xl text-white">Store</h1>
         </div>
-    </>
+    </div>
   );
 }
